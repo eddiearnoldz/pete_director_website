5e9eea71e371db631140a657297b077d46e2f9d3
eddie arnold <eddiearnoldz@mac.com>
2025-06-12 17:36:36 -0700

    podcast image now has alt tag

--- src/views/PodcastView.vue
+++ src/views/PodcastView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="podcast container">
-    <img class="fade-in" src="/assets/images/CandyBang.webp" alt="">
+    <img class="fade-in" src="/assets/images/CandyBang.webp" alt="Never Stop Candy Bang Podcast Logo">
     <h1 class="fade-in">coming soon...</h1>
   </div>
 </template>
